8b1c8c9c624b566747f8cd4dcbc1c7ec30547363
NishantRout <nishantnarayanrout@gmail.com>
2020-11-05 18:19:07 +0530

    ribbon

--- src/components/Iphone.js
+++ src/components/Iphone.js
@@ -1,10 +1,22 @@
 import React from "react";
 import DeviceMenu from "./DeviceMenu";
+import "./css/Iphone.css";
+import Iphone12pro from "./Iphone12pro";
 
 function Iphone() {
   return (
     <div className="iphone">
       <DeviceMenu />
+
+      <div className="iphone__ribbon">
+        <p>
+          Shop now at the Apple Store online with many great ways to buy. Get
+          free, no-contact delivery, Specialist help and{" "}
+          <span className="iphoneRibbon__more">more &#62;</span>
+        </p>
+      </div>
+
+      <Iphone12pro />
     </div>
   );
 }
---
+++ src/components/Iphone12pro.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/Iphone12pro.css";
+
+function Iphone12pro() {
+  return (
+    <div className="iphone2pro">
+      <h1>iphone</h1>
+    </div>
+  );
+}
+
+export default Iphone12pro;
